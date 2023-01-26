595046798b63732e8a2d16bfdbbedc688222335d
Bailee <103554611+thebaiway@users.noreply.github.com>
2023-01-26 09:51:43 -0800

    updating logic

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+
+  const [homeScore, setHomeScore] = useState(0)
+
+  function addOneHome () {
+    setHomeScore(prevHomeScore => prevHomeScore + 1)
+  }
+
+  function addTwoHome () {
+    setHomeScore(prevHomeScore => prevHomeScore + 2)
+  }
+
+  function addThreeHome () {
+    setHomeScore(prevHomeScore => prevHomeScore + 3)
+  }
+
+  const [awayScore, setAwayScore] = useState(0)
+
+  function addOneAway () {
+    setAwayScore(prevAwayScore => prevAwayScore + 1)
+  }
+
+  function addTwoAway () {
+    setAwayScore(prevAwayScore => prevAwayScore + 2)
+  }
+
+  function addThreeAway () {
+    setAwayScore(prevAwayScore => prevAwayScore + 3)
+  }
+
+  function gameReset () {
+    setHomeScore(prevHomeScore => prevHomeScore = 0)
+    setAwayScore(prevAwayScore => prevAwayScore = 0)
+  }
+
   return (
     <div className="App">
        <div class="container">
         <div>
             <h3>HOME</h3>
-            <p id="score-el-home">0</p>
+            <p id="score-el-home">{homeScore}</p>
             <div class="increment-div">
-                <span onclick="addOneHome()">+1</span>
-                <span onclick="addTwoHome()">+2</span>
-                <span onclick="addThreeHome()">+3</span>
+                <span onClick = {addOneHome}>+1</span>
+                <span onClick = {addTwoHome}>+2</span>
+                <span onClick = {addThreeHome}>+3</span>
             </div>
         </div>
         <div>
             <h3>AWAY</h3>
-            <p id="score-el-guest">0</p>
+            <p id="score-el-guest">{awayScore}</p>
             <div class="increment-div">
-                <span onclick="addOneGuest()">+1</span>
-                <span onclick="addTwoGuest()">+2</span>
-                <span onclick="addThreeGuest()">+3</span>
+                <span onClick = {addOneAway}>+1</span>
+                <span onClick = {addTwoAway}>+2</span>
+                <span onClick = {addThreeAway}>+3</span>
             </div>
         </div>
     </div>
     <div id="new-game-div">
-        <h1 onclick="gameReset()">New Game?</h1>
+        <h1 onClick = {gameReset}>New Game?</h1>
     </div> 
     </div>
   );
